d0a8236c86e941de85b094b5d0021d0aca9ee666
Sachin <99914649+gk-sachin@users.noreply.github.com>
2023-07-28 11:40:53 +0530

    Completed

---
+++ Day - 11/src/home.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import Hero from "./hero/hero.js"
+import Program from "./programs/program";
+
+function Homepage() {
+  
+    return ( 
+        <>
+         
+         
+
+          <Hero/>
+          <Program />
+       
+    </> 
+     );
+}
+
+export default Homepage;
